13f0f429250a092ee51b4877241ccfd1a265d1d7
dharmendra kumar <technicalradhe@gmail.com>
2024-06-16 20:49:17 +0530

    Created featured component

--- src/components/AnimatedBorderCard.tsx
+++ src/components/AnimatedBorderCard.tsx
@@ -1,14 +1,16 @@
- 'use client'
+'use client'
 import React from 'react'
 import { BackgroundGradient } from "./ui/background-gradient";
-const AnimatedBorderCard = () => {
-  return (
-    <div>
- <BackgroundGradient className="rounded-[22px] bg-white dark:bg-zinc-900">
-      sd
-      </BackgroundGradient>
-    </div>
-  )
+const AnimatedBorderCard = ({ children }: {
+    children: React.ReactNode
+}) => {
+    return (
+        <div>
+            <BackgroundGradient className="rounded-[22px] overflow-hidden bg-white dark:bg-zinc-900">
+                {children}
+            </BackgroundGradient>
+        </div>
+    )
 }
 
 export default AnimatedBorderCard
--- src/components/FeaturedCards.tsx
+++ src/components/FeaturedCards.tsx
@@ -1,32 +1,38 @@
+
+
 import React from 'react'
 import AnimatedBorderCard from './AnimatedBorderCard'
-
+import MusicData from '../data/music_courses.json'
 function FeaturedCards() {
+
+    const featuredCourses = MusicData.courses.filter((course) => course.isFeatured);
+
     return (
         <div>
-            <div className='max-w-7xl mx-auto border'>
+            <div className='container mx-auto border'>
                 <div>
                     <h1 className='text-3xl md:text-6xl py-8 font-bold text-center'>Featured</h1>
                 </div>
-                <div className="grid p-4 gap-6 md:grid-cols-2 lg:grid-cols-3">
-                    <div className=''>
-                        <AnimatedBorderCard></AnimatedBorderCard>
-                    </div>
-                    <div className=''>
-                        <AnimatedBorderCard></AnimatedBorderCard>
-                    </div>
-                    <div className=''>
-                        <AnimatedBorderCard></AnimatedBorderCard>
-                    </div>
-                    <div className=''>
-                        <AnimatedBorderCard></AnimatedBorderCard>
-                    </div>
-                    <div className=''>
-                        <AnimatedBorderCard></AnimatedBorderCard>
-                    </div>
-                    <div className=''>
-                        <AnimatedBorderCard></AnimatedBorderCard>
-                    </div>
+                <div className="grid mx-auto p-4 gap-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+                    {
+                        featuredCourses && featuredCourses.map(course => (
+                            <div key={course.id} className=''>
+                                <AnimatedBorderCard >
+                                    <div className='col-span-1 flex flex-col'>
+                                        <img className='w-full' src={course.image} alt={course.title} />
+                                        <div className='p-4 flex flex-col'>
+                                            <h1 className='text-2xl font-bold'>{course.title}</h1>
+                                            <p className='opacity-50 flex-1'>{course.description}</p>
+                                            <p className='text-blue-500 dark:text-blue-500 text-2xl font-bold'>Price:  ${course.price}</p>
+                                            <p className='text-right'>{course.instructor}</p>
+                                        </div>
+                                    </div>
+
+                                </AnimatedBorderCard>
+                            </div>
+                        ))
+                    }
+
                 </div>
             </div>
         </div>
