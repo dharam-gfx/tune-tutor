426af5dc5cfca9ce71545bf80eef5d38fa850962
kdharmendra <technicalradhe@gmail.com>
2024-06-18 11:14:28 +0530

    format code

--- src/components/NavBAr.tsx
+++ src/components/NavBAr.tsx
@@ -12,17 +12,17 @@ export default function NavBar() {
   );
 }
 
-function Navbar({ className }: { className?: string }) {
-  const [active, setActive] = useState<string | null>(null);
+function Navbar( { className }: { className?: string } ) {
+  const [active, setActive] = useState<string | null>( null );
   return (
     <div
-      className={cn("fixed top-10 inset-x-0 max-w-2xl mx-auto z-50", className)}
+      className={cn( "fixed top-10 inset-x-0 max-w-2xl mx-auto z-50", className )}
     >
       <Menu setActive={setActive}>
         <Link href="#">
-            <MenuItem setActive={setActive} active={active} item="Home"></MenuItem>
+          <MenuItem setActive={setActive} active={active} item="Home"></MenuItem>
         </Link>
-       
+
         <MenuItem setActive={setActive} active={active} item="Services">
           <div className="flex flex-col space-y-4 text-sm">
             <HoveredLink href="/web-dev">Web Development</HoveredLink>
